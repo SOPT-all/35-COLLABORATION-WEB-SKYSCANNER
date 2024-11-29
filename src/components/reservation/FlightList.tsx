c26ddcbebe201b8b22fec671447ebaa5627475a8
Heesun Park <phs1585@naver.com>
2024-11-29 18:16:59 +0900

    feat: 무한 스크롤 구현

--- src/components/reservation/FlightList.tsx
+++ src/components/reservation/FlightList.tsx
@@ -1,18 +1,74 @@
 import FlightCard from '@/components/reservation/FlightCard';
+import { fetchMoreFlights, useFlights } from '@/hooks/useFlights';
 import { Flights } from '@/types/FlightTypes';
+import { useCallback, useEffect, useState } from 'react';
+import styled from 'styled-components';
 
-interface FlightListProps {
-	flights: Flights[];
-}
+const FlightList = () => {
+	// 초기 항공권 데이터, 5개
+	const { flights: initialFlights } = useFlights();
 
-const FlightList = ({ flights }: FlightListProps) => {
+	// 무한 스크롤 상태
+	const [loading, setLoading] = useState(false);
+	const [flights, setFlights] = useState<Flights[]>([]);
+	const [initialLoad, setInitialLoad] = useState(true);
+
+	// 처음 데이터 로딩 처리
+	useEffect(() => {
+		if (initialFlights.length > 0 && initialLoad) {
+			setFlights(initialFlights);
+			setInitialLoad(false);
+		}
+	}, [initialFlights, initialLoad]);
+
+	// 데이터를 더 불러옴
+	const loadMoreFlights = useCallback(async () => {
+		if (loading) return;
+		setLoading(true);
+
+		const moreFlights = await fetchMoreFlights();
+		setFlights((prevFlights) => [...prevFlights, ...moreFlights]);
+
+		setLoading(false);
+	}, [loading]);
+
+	// 스크롤 이벤트 핸들러
+	const handleScroll = useCallback(
+		(event: React.UIEvent<HTMLElement>) => {
+			const target = event.target as HTMLElement;
+			const { scrollTop, clientHeight, scrollHeight } = target;
+
+			if (scrollTop + clientHeight >= scrollHeight - 1 && !loading) {
+				loadMoreFlights();
+			}
+		},
+		[loading, loadMoreFlights],
+	);
+
 	return (
-		<>
+		<FlightContainer onScroll={handleScroll}>
 			{flights.map((flight) => (
 				<FlightCard key={flight.id} flight={flight} />
 			))}
-		</>
+			{loading && <LoadingIndicator>로딩 중...</LoadingIndicator>}
+		</FlightContainer>
 	);
 };
 
 export default FlightList;
+
+const FlightContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 0.9rem;
+	overflow-y: auto;
+	max-height: 72.5rem;
+	min-height: 30rem;
+`;
+
+const LoadingIndicator = styled.div`
+	text-align: center;
+	padding: 1rem;
+	color: ${({ theme }) => theme.colors.grey40};
+	font-size: 1%.5;
+`;
